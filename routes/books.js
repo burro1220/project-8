e7f993326b10bfd195d356b6fd1d53c9341e9c2b
Blake Burroughs <b.burro@gmail.com>
2019-04-30 12:18:21 -0500

    completed CRUD functionality

--- routes/books.js
+++ routes/books.js
@@ -28,28 +28,15 @@ router.post('/', function(req, res, next) {
 
 /* Create a new book form. */
 router.get('/new', function(req, res, next) {
-  res.render("books/new", {book: {}, title: "New Book"});
+  res.render("books/new-book", {book: {}, title: "New Book"});
 });
 
-/* Delete book form. */
-router.get("/:id/delete", function(req, res, next){
-    Book.findById(req.params.id).then(function(book){  
-      if(book) {
-        res.render("books/delete", {book: book, title: "Delete Book"});
-      } else {
-        res.send(404);
-      }
-    }).catch(function(error){
-        res.send(500, error);
-     });
-  });
-  
   
 /* GET individual book. */
 router.get("/:id", function(req, res, next){
-    Book.findById(req.params.id).then(function(book){
+    Book.findByPk(req.params.id).then(function(book){
     if(book) {
-        res.render("books/show", {book: book, title: book.title});  
+        res.render("books/update-book", {book: book, title: book.title});  
     } else {
         res.send(404);
     }
@@ -58,9 +45,32 @@ router.get("/:id", function(req, res, next){
     });
   });
 
+  /* PUT update book. */
+router.put("/:id", function(req, res, next){
+  Book.findByPk(req.params.id).then(function(book){
+    if(book) {
+      return book.update(req.body);
+    } else {
+      res.send(404);
+    }
+  }).then(function(book){
+    res.redirect("/books/" + book.id);        
+  }).catch(function(error){
+      if(error.name === "SequelizeValidationError") {
+        const book = Book.build(req.body);
+        book.id = req.params.id;
+        res.render("books/update-book", {book: book, errors: error.errors, title: "Edit Book"})
+      } else {
+        throw error;
+      }
+  }).catch(function(error){
+      res.send(500, error);
+   });
+});
+
 /* DELETE individual book. */
 router.delete("/:id", function(req, res, next){
-    Book.findById(req.params.id).then(function(book){  
+    Book.findByPk(req.params.id).then(function(book){  
       if(book) {
         return book.destroy();
       } else {
